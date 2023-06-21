b7568d49a3ba83bacb9d91ea1137b1eafe898945
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-21 15:34:31 +0900

    Create 1620.py

---
+++ 단계별로 풀어보기/집합과 맵/1620.py
@@ -0,0 +1,23 @@
+'''
+나는야 포켓몬 마스터 이다솜
+https://www.acmicpc.net/submit/1620/62286831
+'''
+
+import sys
+
+#input() 사용 시 시간 초과 발생
+n, m = map(int, sys.stdin.readline().split())
+
+dic = {}
+for i in range(n):
+    # readline()으로 입력받을 시 생기는 "\n" 문자를 제거하기 위해 .rstrip() 필요
+    a = sys.stdin.readline().rstrip()
+    dic[a] = i+1
+    dic[i+1] = a
+
+for i in range(m):
+    poc = sys.stdin.readline().rstrip()
+    if poc.isdigit():
+        print(dic[int(poc)])
+    else:
+        print(dic[poc])
