4c92d18eafbd0ef6eec944ba97d16a9eb1474f4c
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-21 15:28:27 +0900

    Create 숫자 카드.py

---
+++ 단계별로 풀어보기/집합과 맵/숫자 카드.py
@@ -0,0 +1,34 @@
+'''
+https://www.acmicpc.net/problem/10815
+'''
+
+n = int(input())
+int_num = list(map(int, input().split()))
+m = int(input())
+num_m  = list(map(int, input().split()))
+
+#딕셔너리 사용
+dic = {}
+for i in int_num:
+    dic[i] = 1
+
+for i in num_m:
+    if i in dic:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+        
+'''
+# 리스트로 할 경우 시간초과 발생
+
+n = int(input())
+int_num = list(map(int, input().split()))
+m = int(input())
+num_m  = list(map(int, input().split()))
+
+for i in range(m):
+    if(num_m[i] in int_num):
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+ '''
