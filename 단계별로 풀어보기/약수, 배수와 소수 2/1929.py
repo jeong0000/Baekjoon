c5d469e8d8b07811353953667c11a23e9ba99fc5
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-22 16:15:26 +0900

    Update 1929.py

--- 단계별로 풀어보기/약수, 배수와 소수 2/1929.py
+++ 단계별로 풀어보기/약수, 배수와 소수 2/1929.py
@@ -3,3 +3,22 @@
 https://www.acmicpc.net/problem/1929
 '''
 
+import math
+
+m, n = map(int, input().split())
+
+# 소수는 1과 자기자신을 제외한 약수가 존재하지 않는 경우를 말함
+# 약수들은 대칭적으로 짝을 이루기 때문에 자기 자신의 제곱근(루트)까지만 확인하면 됨
+# ex) 12 -> 1 * 12, 2 * 6, 3 * 4, 4 * 3, 6 * 2, 12 * 1
+def primeNum(k):
+    for i in range(2, int(math.sqrt(k)) + 1):
+        if k % i == 0:
+            return False
+    return True
+
+if(m == 1):
+    m = 2
+
+for i in range(m, n+1):  
+    if primeNum(i) == True:
+        print(i)
