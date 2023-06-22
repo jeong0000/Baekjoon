2af940032d51290ed116247fac3a68bc168f2f55
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-22 11:48:11 +0900

    Update 1934.py

--- 단계별로 풀어보기/약수, 배수와 소수 2/1934.py
+++ 단계별로 풀어보기/약수, 배수와 소수 2/1934.py
@@ -3,3 +3,20 @@
 https://www.acmicpc.net/problem/1934
 '''
 
+t = int(input())
+
+# 유클리드 알고리즘
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a%b)
+        
+for i in range(t):
+    a, b = map(int, input().split())
+    if(a<b):
+        a, b = b, a
+    
+    result = (a * b) // gcd(a, b)
+
+    print(result)
