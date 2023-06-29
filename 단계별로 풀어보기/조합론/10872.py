30357d49fad43c9b96fad0ecf3182032d94bfb3d
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-29 16:25:24 +0900

    Update 10872.py

--- 단계별로 풀어보기/조합론/10872.py
+++ 단계별로 풀어보기/조합론/10872.py
@@ -3,6 +3,9 @@
 https://www.acmicpc.net/problem/10872
 '''
 
+# 0! = 1
+# (n-1)! = n! / n
+
 n = int(input())
 
 result = 1
@@ -10,8 +13,3 @@ for i in range(2, n+2):
     result *= i
 
 print(result//(n+1))
-
-'''
-0! = 1
-(n-1)! = n! / n
-'''
