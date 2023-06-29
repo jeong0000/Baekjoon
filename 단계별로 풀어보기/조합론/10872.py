3d9606b4c3227f33d2ea5abea48472e6a5498eca
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-29 16:08:06 +0900

    Create 10872.py

---
+++ 단계별로 풀어보기/조합론/10872.py
@@ -0,0 +1,17 @@
+'''
+팩토리얼
+https://www.acmicpc.net/problem/10872
+'''
+
+n = int(input())
+
+result = 1
+for i in range(1, n+2):
+    result *= i
+
+print(result//(n+1))
+
+'''
+0! = 1
+(n-1)! = n! / n
+'''
