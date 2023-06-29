3a27a82d179a43f910adac6002fe328b23375e5d
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-06-29 16:24:51 +0900

    Create 11050.py

---
+++ 단계별로 풀어보기/조합론/11050.py
@@ -0,0 +1,19 @@
+'''
+이항 계수 1
+https://www.acmicpc.net/problem/11050
+'''
+
+#이항계수 : 주어진 크기 집합에서 원하는 개수만큼 순서없이 뽑는 조합의 가짓수 -> nCk
+#nCk = n!/r!(n-r)!
+
+n, k = map(int, input().split())
+
+def Febo(a):
+    result = 1
+    for i in range(2, a+2):
+        result *= i
+    
+    result /= (a+1)
+    return result
+
+print(int(Febo(n) / (Febo(k) * Febo(n-k))))
