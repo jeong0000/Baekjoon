6bbdbfeb6f362f1e078c5aebff7ed6817328ac25
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-07-05 11:41:51 +0900

    Create 4949.py

---
+++ 단계별로 풀어보기/스택/4949.py
@@ -0,0 +1,31 @@
+'''
+균형잡힌 세상
+https://www.acmicpc.net/problem/4949
+'''
+
+import sys
+
+while True:
+    a = sys.stdin.readline().rstrip()
+
+    if a == ".":
+        break
+
+    stack = []
+    for i in a:
+        if i == "(" or i == "[":
+            stack.append(i)
+        elif i == ")" or i == "]":
+            if len(stack) == 0:
+                stack.append(0)
+                break
+            elif (i == ")" and stack[len(stack)-1] == "(") or (i == "]" and stack[len(stack)-1] == "["):
+                del stack[len(stack)-1]
+            else:
+                stack.append(0)
+                break
+
+    if len(stack) == 0:
+        print("yes")
+    else:
+        print("no")
