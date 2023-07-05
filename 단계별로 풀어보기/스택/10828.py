d287a899042ea55948c60f2adaa0e48912b88304
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-07-05 10:13:53 +0900

    Create 10828.py

---
+++ 단계별로 풀어보기/스택/10828.py
@@ -0,0 +1,33 @@
+'''
+스택
+https://www.acmicpc.net/problem/10828
+'''
+
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+for _ in range(n):
+    order= list(map(str, sys.stdin.readline().split()))
+
+    if(order[0] == 'push'):
+        stack.append(order[1])
+    elif(order[0] == 'pop'):
+        if len(stack) != 0:
+            print(stack[len(stack)-1])
+            del stack[len(stack)-1]
+        else:
+            print(-1)
+    elif(order[0] == 'size'):
+        print(len(stack))
+    elif(order[0] == 'empty'):
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif(order[0] == 'top'):
+        if len(stack) != 0:
+            print(stack[len(stack)-1])
+        else:
+            print(-1)
