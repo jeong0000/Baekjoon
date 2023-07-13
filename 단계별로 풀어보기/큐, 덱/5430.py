94603ae8c8cc98acb9d36701fdc6bf2cf15c36ea
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-07-13 14:32:15 +0900

    Create 5430.py

---
+++ 단계별로 풀어보기/큐, 덱/5430.py
@@ -0,0 +1,38 @@
+'''
+AC
+https://www.acmicpc.net/problem/5430
+'''
+
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    p = sys.stdin.readline().rstrip()
+    n = int(sys.stdin.readline())
+    queue = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
+    
+    if n == 0:
+        queue = []
+
+    flag = 0
+    for i in p:
+        if i == "R":
+            # reverse() 사용 시 시간초과 발생
+            # reverse()의 시간복잡도 : O(n)
+            # for 문 안에서의 사용이기에 시간복잡도 -> O(n^2)
+            flag += 1
+        elif i == "D":
+            if queue:
+                if flag % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
+            else:
+                print("error")
+                break
+    else:
+        if flag % 2 != 0:
+            queue.reverse()
+        print("[" + ",".join(queue) + "]")
