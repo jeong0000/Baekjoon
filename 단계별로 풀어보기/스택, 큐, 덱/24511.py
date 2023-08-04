71c18591aef31c5837da68500ec096f0a0297304
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-08-04 11:49:29 +0900

    Update 24511.py

--- 단계별로 풀어보기/스택, 큐, 덱/24511.py
+++ 단계별로 풀어보기/스택, 큐, 덱/24511.py
@@ -3,20 +3,52 @@ queuestack
 https://www.acmicpc.net/problem/24511
 '''
 
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+m = int(input())
+c = list(map(int, input().split()))
+
+queue = deque()
+for i in range(n):
+    # 스택인 경우에는 입력, 출력값이 같아 변화 X => 큐인 경우만 실행
+    # c[i]값이 들어오면 b[i] 값 출력
+    if a[i] == 0:
+        queue.appendleft(b[i])
+
+# a가 다 스택(1)일 경우
+else:
+    if len(queue) == 0:
+        # c 리스트 그대로 출력
+        print(*c)
+        # 코드 즉시 종료
+        sys.exit()
+
+for i in range(m):
+    queue.append(c[i])
+    print(queue.popleft(), end=" ")
 
 '''
 
 첫번째 풀이 -> 시간초과
+
 import sys
 from collections import deque
 
-n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
-b = deque(list(map(int, sys.stdin.readline().split())))
+input = sys.stdin.readline
 
-m = int(sys.stdin.readline())
-c = list(map(int, sys.stdin.readline().split()))
+n = int(input())
+a = list(map(int, input().split()))
+b = deque(list(map(int, input().split())))
 
+m = int(input())
+c = list(map(int, input().split()))
 
 for i in c:
     tmp = i
