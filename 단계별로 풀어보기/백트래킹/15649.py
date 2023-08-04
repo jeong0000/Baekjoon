1f89f6b438896a47b5cad9f82395b2b13a373c3f
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-08-04 13:19:52 +0900

    Update 15649.py

--- 단계별로 풀어보기/백트래킹/15649.py
+++ 단계별로 풀어보기/백트래킹/15649.py
@@ -4,3 +4,23 @@ https://www.acmicpc.net/problem/15649
 '''
 
 
+'''
+
+다른 풀이 - itertools의 permutations 함수 사용(파이썬만 가능)
+
+import sys, itertools
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+ary = [i for i in range(1, n+1)]
+
+c = itertools.permutations(ary, m)
+
+for i in c:
+    for j in i:
+        print(j, end=" ")
+    print()
+
+## combinations : 중복 가능
+    
+'''
