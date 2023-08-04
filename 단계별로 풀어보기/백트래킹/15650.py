a3a36de254c099be0626e3e41365d67a9e973c8a
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-08-04 14:41:59 +0900

    Create 15650.py

---
+++ 단계별로 풀어보기/백트래킹/15650.py
@@ -0,0 +1,25 @@
+'''
+N과 M (2)
+https://www.acmicpc.net/problem/15650
+'''
+#백트래킹 입문 문제 2
+
+import sys
+input = sys.stdin.readline
+
+def btk():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n+1):
+        if i in s:
+            continue
+        if len(s) == 0 or i > s[len(s)-1]:
+            s.append(i)
+            btk()
+            s.pop()
+
+n, m = map(int, input().split())
+s = []
+btk()
