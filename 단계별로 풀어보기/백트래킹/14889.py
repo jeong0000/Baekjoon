b90049f02213c59a885d45b8d22d0903fd6204a2
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-08-17 14:38:53 +0900

    Create 14889.py

---
+++ 단계별로 풀어보기/백트래킹/14889.py
@@ -0,0 +1,39 @@
+'''
+스타트와 링크
+https://www.acmicpc.net/problem/14889
+'''
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = []
+for i in range(n):
+    s.append(list(map(int, input().split())))
+
+# 방문 리스트
+visited = [False for _ in range(n)]
+min_n = 100
+
+def btk(depth, i):
+    global min_n
+
+    if depth == n//2:
+        start, link = 0, 0
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:
+                    start += s[i][j]
+                elif not visited[i] and not visited[j]:
+                    link += s[i][j]
+        min_n = min(min_n, abs(start - link))
+        return
+    
+    for i in range(i, n):
+        if not visited[i]:
+            visited[i] = True
+            btk(depth+1, i+1)
+            visited[i] = False
+
+btk(0,0)
+print(min_n)
