7c97115da9b52cb52589b2b2a4c4e37db7970760
jeong0000 <73898201+jeong0000@users.noreply.github.com>
2023-10-26 15:31:52 +0900

    Update 1269.py

--- 단계별로 풀어보기/집합과 맵/1269.py
+++ 단계별로 풀어보기/집합과 맵/1269.py
@@ -9,5 +9,21 @@ a, b = map(int, input().split())
 arr_1 = set(list(map(int, input().split())))
 arr_2 = set(list(map(int, input().split())))
 
-dif = len(arr_1) - len(arr_1 - arr_2)
-print(len(arr_1) + len(arr_2) - 2*dif)
+print(len(arr_1 ^ arr_2))
+
+'''
+교집합
+print(arr_1 & arr_2)
+print(arr_1.intersaction(arr_2))
+
+합집합
+print(arr_1 | arr_2)
+print(arr_1.union(arr_2))
+
+차집합
+print(arr_1 - arr2)
+print(arr_1.difference(arr_2))
+
+대칭 차집합
+print(arr_1 ^ arr_2)
+'''
